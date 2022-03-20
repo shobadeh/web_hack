2310216631e8dd3ba5a0e8d243ed25f40c480e25
mr Shobadehgar <101979989+shobadeh@users.noreply.github.com>
2022-03-20 17:12:52 +0330

    Add files via upload

---
+++ local.py
@@ -0,0 +1,168 @@
+import os
+import time
+import sys
+import colored ,colorama
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style
+
+os.system("clear")
+print (Fore.BLUE + "")
+f = """ mr shobadeh or shobadeh gar Script for testing locales in localhost made by master shobadeh gar
+
+the channel: rubika.ir/caetorr
+
+the rules  : rules-magic.blogfa.com
+"""
+for h in f:
+    sys.stdout.write(h)
+    sys.stdout.flush()
+    time.sleep(0.1)
+print(Fore.RED + "")
+print (" ")
+ok = input ("""
+
+Hack Web With list password    >> -1-
+
+Source Web inside of Localhost >> -2-
+
+ 
+          type numbers -->>""")
+if ok == "1":
+    print ("OK")
+    os.system("bash mr.sh")
+   
+if ok == "2":
+    print("ok")
+    
+time.sleep(1)
+try:
+	import requests
+except ImportError:
+	print('Requests Module Not Found !!')
+	imp = input('Do You Want To Install Requests? y/n ')
+	if imp=='y' or 'Y':
+		import os
+		os.system('pip install requests')
+		os.system('clear')
+		os.system('clear')
+		os.system('clear')
+		print('Installation Completed!')
+		print('Now Checking Flask Module')
+try:
+	from flask import Flask
+	print('Mr Shobadehgar Starting..... ')
+	time.sleep(.45)
+	os.system('clear')
+except ImportError:
+	print('Flask Module Not Found ')
+	imp = input('Do You Want To Install Flask? y/n ')
+	if imp=='y' or 'Y':
+		import os
+		os.system('pip install flask')
+		os.system('clear')
+		os.system('clear')
+		os.system('clear')
+		print('Installation Completed')
+	else:
+		exit();
+#Importing
+import time
+print (Fore.YELLOW + "")
+col = '\033[1;31;40m'
+def logo():
+	a = """
+                  
+             .7KBQBBBBBBBBBIr
+                        i5RQQP1ri..   ..i7IbQQM2i
+                    rQBBDY.                   .JgBBMr
+                 rBQB7                             vBBBi
+               PBBr                                   vBQI
+             ZBD.                                       .QBX
+           IBg                                             BBv
+          BB.                                               .BB
+        vBY      qq.       ...:r::i.::i::i:..        :dI      XBi
+       DB     :BBR      ..: .:.  :  : ..  ::....       BBB.    .B2
+      BB   g:1BBi     .r:   .   ..  :  :.   .   :i.     7BQ7rd   Bb
+     gB   BE.BIi:   .:. ..:i.   i   i   :   .i:.. .:.   i:PB BB   BI
+    YB   BB :iIB   :.    ..  ..:: dd7BQ:.:..  ..    .:  .BJi. Qd   Bi
+    B  B BQ QBB   i     .:     :  BR XBB .     :.     i   BBD BB.B :B
+   BM PB BQBU:   :      i      i    :BM  i      i      i   iXBBB Bv BB
+  .B  BB Qu P7  i....  :.     ..    X    :      ..  ....i  u1 qg BB  B
+  BB  BB  rBB  i    ...i....  :.   .5    :. .....i...    :  BBi .BB  Bj
+  B   BB DBZ  .:      ..   .i...# maxtor #...: .    :       :   BBI.BZ  iB
+ jQ Y: BQB..  :       :.      :           .      ..      .:   :BMB Y: B
+ BB MB RB  B  i       :       .   rEXEi   .       :       :  B  BS B1 B
+ BZ vB: : BP  i.......:        1B .QBd  Bv       .:.......:  BB r vB: B
+ BM  BB  BB   i . . . :  .:rUZBBB  rB:  BBBE1i:.  : ... . i   BB .BB  B
+ BB  KBgiBY   :        rBBBBBBQB.   Q   iBBBBBBBBi        :   KB:BBL  B
+ vB L YBBB Qi :        BBBBQBBBB:  5B7  7BBBBBBBBZ       .: LK BQQi r B
+  B rB  BR Bd  :       BQBQBQBBBQ  MQS  QBQBBBBBBB       :  BB BB  B.:B
+  QQ BB. : QB  :    . :BBBBBBBBBB7 bBs 5BBBBBBBBBB. .   ..  BB i :BB BY
+   B  BBB  BQ i :...  :BBBBBQBBBBB:KBJiBBBBBBBBBBB   ..:. r BB  BBQ  B
+   BB  uBBiBB Bj .    PBBQBBBBBQBBBBBQBBBBBBBQBQBBJ    . dB BRrBB7  BQ
+    B. 7 vBBQ rB: .   QBBBBBBBBBBBBBQBBBBBBBBBBBBBZ   . 7B..BBBr 7 7B
+    7B iB.  K5 BB     BQBBBBBQBQBBBBBBBQBBBBBBBBBBB     BB D1  iB. Q:
+     bQ .BBP:: YBd 5  BBQBBBBBBBQBQBBBQBBBBBQBBBBBB  X BBi ::gBB  BJ
+      gB  rBBBBrZB.uB7BBBBBBBBBQBBBBBQBBBQBBBBBBBQBrBi:QqrBBBQ:  BI
+       KB.  ivqBMBB 1BBBBBBBBBBBBBBBBBBBBBBBBBBBBBQB7 BBMBSvi  :BY
+        rQ5 .u7:. :r .KQBBBBBBBBBQBQBBBBBBBBBBBQBQS  7: .:vJ  DB:
+          QB  iBBBBBBBBBQKrQQBBBBBBBBBBBBBBBQZrPQBBBBBBBBQ: .BB
+           7Bg   :i:..    iQBBBBBBBQBQBBBBBBBRi    ..:r.   BBi
+             XBR  .2ZQBBBBBBQBBBBBBBBBBBBBBBQBBBBBBQE1. .BBu
+               uBQi  .r7i  BBQBBBBBQBBBBBBBBBQ .rri.  rBBv
+                 :gBgi     BQBBBBBBBQBBBBBBBBB     rMBZ.
+                    idBBgrJBBBBBBBBBBBBBBBBBBB77QBBP:
+                        .rv1XZMQQBBBBBBBQQgZSu7i 
+
+
+shobadeh gar
+"""
+	print(a)
+	print(col + 'MR SHOBADEHGAR')
+	print('   This Program Is Created For Testing Purposes Dont Use It For Any Illegal Purposes!')
+time.sleep(1)
+os.system('clear')
+time.sleep(1)
+os.system('clear')
+print('\n>>>>>>>>>>>>>>>>rules-magic.blogfa.com<<<<<<<<<<<<<<<<')
+
+logo()
+print('')
+
+site = input('Enter Website URL do>>>_ ')
+try:
+	req = requests.get(site)
+	src = req.text
+	time.sleep(.34)
+	print('Getting Websites Source Code')
+except:
+	print('Url Not Found Or Your Internet Is Not Available')
+	exit();
+with open('src.html', 'w') as sorc:
+	r = sorc.write(src)
+time.sleep(.65)
+print('Saving Source Code As src.txt')
+time.sleep(.23)
+print('Moving The File To Template Folder ')
+os.system('mv -f src.html templates')
+time.sleep(.65)
+print('File Moved Successfully!')
+time.sleep(.45)
+os.system('clear')
+logo()
+print("")
+vol= '\033[3;37;40m'
+print('Starting LocalHost At Port 5000')
+print(vol + 'Localhost Started At Port Number 5000\nOpen 127.0.0.1/5000 In Your Browser To See Website')
+print('\n')
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	return render_template('src.html')
+
+app.run()
+os.system('clear')
+logo()
+print('')
